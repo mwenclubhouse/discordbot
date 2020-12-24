6a62dcbb13a16de35952dc10782eb1c4f74282b5
Matthew Wen <mattwen2018@gmail.com>
2020-12-24 03:48:49 -0600

    create service change environment variable

--- script/create_service.sh
+++ script/create_service.sh
@@ -9,7 +9,7 @@ echo "[Unit] " >> $app_name
 echo "Description=Python Script For Discord Bot" >> $app_name
 echo "[Service]" >> $app_name
 echo "Type = simple" >> $app_name
-echo "Environment=GOOGLE_APPLICATION_CREDENTIALS=$HOME/.firebase/firebase-key.json" >> $app_name
+echo "Environment=GOOGLE_APPLICATION_CREDENTIALS=$HOME/.firebase/mwenclubhouse.json" >> $app_name
 echo "ExecStart = $current_dir/main" >> $app_name
 sudo cp $app_name /etc/systemd/system/
 sudo chmod 644 /etc/systemd/system/$app_name
