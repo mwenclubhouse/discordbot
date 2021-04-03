b134c7243cd910d0c44c9e6d236dc8cd36a522fe
Matthew <mattwen2018@gmail.com>
2021-04-03 19:27:18 +0000

    Fixing the Done to wrap calendar

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name='mwenclubhouse-bot',
+    scripts=["bin/mwenclubouse-discord"],
     packages=['mwenclubhouse_discord'],
     version='1.0',
     author="Matthew Wen",
