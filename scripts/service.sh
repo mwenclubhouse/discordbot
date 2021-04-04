45fc8caa1f759469d34f8eee97207a1c019d5de2
Matthew Wen <mattwen2018@gmail.com>
2021-04-04 05:37:54 -0400

    restarts the right service

--- scripts/service.sh
+++ scripts/service.sh
@@ -5,6 +5,6 @@ cd ../
 mkdir -p ~/.config/systemd/user/
 cp service/discord-bot.service ~/.config/systemd/user/
 systemctl daemon-reload --user
-systemctl restart queueup-bot.service --user
+systemctl restart discord-bot.service --user
 loginctl enable-linger $USER
 exit
