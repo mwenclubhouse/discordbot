a619dccc996c0464fed11768eefcf407d6012710
Matthew Wen <mattwen2018@gmail.com>
2021-04-05 18:56:40 -0400

    Created development Branch

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")" || exit
 cd ../
 git fetch origin
-git reset --hard origin/main
+git reset --hard origin/development
 pip uninstall mwenclubhouse-bot -y
 python3 setup.py install --user
 exit
